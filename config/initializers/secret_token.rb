d84d2bf148efc6a9767b5285de6750620eca2380
Paul W <zigloo99@gmail.com>
2011-06-24 21:23:24 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PdlPrescription::Application.config.secret_token = '87ad7bc2585ecc313a75f9689cf2e86a2ded502a0d446bd5e316771c047d813909ad6bba0c8e5708f2c91cd1b5c88e89141029920c17538d453226ebcb8b8067'
