4f82d07deb3159db23bc324946fd1bd391ad2761
Paul W <zigloo99@gmail.com>
2011-07-02 18:30:43 -0400

    added two tests to user_spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,14 @@
 class User
   include MongoMapper::Document
   key :full_name, String
-  key :email_address, String
+  key :email, String
 
   one :patient
-  one :general_dentist
     
   validates :full_name,     :presence => true,
             :length => {:maximum => 50}
             
-  validates :email_address, :presence => true,
-
+  validates :email, :presence => true
     
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @attr = { :full_name => "Example User", :email => "user@example.com"}
+  end
+  
+  it "should create a new instance given valid attributes" do
+    User.create!(@attr)
+  end
+  
+  it "should require a name" do
+    no_name_user = User.new(@attr.merge(:full_name => ""))
+    no_name_user.should_not be_valid
+  end
+  
+  it "should require an email address" do
+    no_email_user = User.new(@attr.merge(:email => ""))
+    no_email_user.should_not be_valid
+  end
+
 end
